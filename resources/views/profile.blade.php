0eac4db5a78505f99647a924ff4eec84cb9d7f7e
ZaretaR <152478352+ZaretaR@users.noreply.github.com>
2024-05-09 00:56:11 +0200

    routes werken

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="nl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profile</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body>
+    <header>
+        <div class="container-nav">
+            @include('includes/nav')
+        </div>
+    </header>
+
+    @include('includes/footer')
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BlogController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TeamController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\KalenderController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +19,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {return view('index');})->name('home');
-Route::get('/team', function () {return view('team');})->name('team');
-Route::get('/contact', function () {return view('contact');})->name('contact');
-Route::get('/kalender', function () {return view('kalender');})->name('kalender');
-Route::get('/blog', function () {return view('blog');})->name('blog');
-Route::get('/profile', function () {return view('profile');})->name('profile');
+Route::get('/', [HomeController::class, 'home'])->name('home');
+Route::get('/team', [TeamController::class, 'team'])->name('team');
+Route::get('/contact', [ContactController::class, 'contact'])->name('contact');
+Route::get('/kalender', [KalenderController::class, 'kalender'])->name('kalender');
+Route::get('/blog', [BlogController::class, 'blog'])->name('blog');
+Route::get('/profile', [ProfileController::class, 'profile'])->name('profile');

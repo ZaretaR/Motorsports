8f71e42e0085f4049f15623066fe3afaf5766c28
ZaretaR <152478352+ZaretaR@users.noreply.github.com>
2024-05-09 22:09:09 +0200

    errors aangepast

--- resources/views/includes/contact-form.blade.php
+++ resources/views/includes/contact-form.blade.php
@@ -6,32 +6,36 @@
     @endif
 
     <div>
-        Naam <br>
-        <input type="text" name="naam" placeholder="Naam" value="{{ old('naam') }}"><br>
+        Naam:
         @error('naam')
             <span>{{ $message }}</span>
         @enderror
+        <br>
+        <input type="text" name="naam" placeholder="Naam" value="{{ old('naam') }}"><br>
     </div>
     <div>
-        Voornaam <br>
-        <input type="text" name="voornaam" placeholder="Voornaam" value="{{ old('voornaam') }}"><br>
+        Voornaam:
         @error('voornaam')
             <span>{{ $message }}</span>
         @enderror
+        <br>
+        <input type="text" name="voornaam" placeholder="Voornaam" value="{{ old('voornaam') }}"><br>
     </div>
     <div>
-        Email <br>
-        <input type="text" name="email" placeholder="johndoe@example.com" value="{{ old('email') }}"><br>
+        Email:
         @error('email')
             <span>{{ $message }}</span>
         @enderror
+        <br>
+        <input type="text" name="email" placeholder="johndoe@example.com" value="{{ old('email') }}"><br>
     </div>
     <div>
-        Bericht <br>
-        <textarea name="bericht" id="bericht" cols="30" rows="10" value="{{ old('bericht') }}"></textarea><br>
+        Bericht:
         @error('bericht')
             <span>{{ $message }}</span>
         @enderror
+        <br>
+        <textarea name="bericht" id="bericht" cols="30" rows="10" value="{{ old('bericht') }}"></textarea><br>
     </div>
     <div>
         <input type="submit" value="Verzenden" class="verzenden">

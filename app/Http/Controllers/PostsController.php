bbfc662cfce07e74912cdecf64c6793a8dbfe37c
ZaretaR <152478352+ZaretaR@users.noreply.github.com>
2024-05-15 13:25:58 +0200

    98%

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,52 +2,121 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Event;
+use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
-class FullCalenderController extends Controller
+class PostsController extends Controller
 {
-    public function index(Request $request)
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
     {
-        if($request->ajax())
-        {
-            $data = Event::where('start', '>=', $request->start)
-                ->where('end', '<=', $request->end)
-                ->get(['id', 'title', 'start', 'end']);
-            return response()->json($data);
-        }
-        return view('full-calender');
+        $post = Post::all();
+        return view('posts.index', [
+            'posts' => $post,
+        ]);
     }
 
-    public function action(Request $request)
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
     {
-        if($request->ajax())
-        {
-            if($request->type == 'toevoegen')
-            {
-                $event = Event::create([
-                    'title' => $request->title,
-                    'start' => $request->start,
-                    'end' => $request->end
-                ]);
-                return response()->json($event);
-            }
+        $post = new Post();
+        return view('posts.create', [
+            'post' => $post,
+        ]);
+    }
 
-            if($request->type == 'bijwerken')
-            {
-                $event = Event::find($request->id)->update([
-                    'title' => $request->title,
-                    'start' => $request->start,
-                    'end' => $request->end
-                ]);
-                return response()->json($event);
-            }
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+            'image' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
 
-            if($request->type == 'verwijderen')
-            {
-                $event = Event::find($request->id)->delete();
-                return response()->json($event);
-            }
+        $userId = Auth::id();
+        $post = new Post();
+        $post->title = $request->title;
+        if ($request->hasFile('image')) {
+            $image = $request->file('image');
+            $fileName = time() . '_' . $image->getClientOriginalName();
+            $image->storeAs('public/images', $fileName);
+            $post->image = $fileName;
         }
+        $post->description = $request->description;
+        $post->user_id = $userId;
+        $post->save();
+
+        $numberOfLikes = $post->likes()->count();
+
+        //return redirect('/posts');
+        return view('posts.index', [
+            'posts' => Post::all(),
+            'numberOfLikes' => $numberOfLikes,
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($id)
+    {
+        $post = Post::find($id);
+        return view('posts.show', compact('post'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit($id)
+    {
+        $post = Post::find($id);
+
+        return view('posts.edit', [
+            'post' => $post,
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+            'image' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+
+        $post = Post::find($id);
+        if ($request->hasFile('image')) {
+            $image = $request->file('image');
+            $fileName = time() . '_' . $image->getClientOriginalName();
+            $image->storeAs('public/images', $fileName);
+            $post->image = $fileName;
+        }
+        $post->title = $request->title;
+        $post->description = $request->description;
+        $post->save();
+
+        return redirect()->route('posts.show', ['id' => $post->id]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($id)
+    {
+        $post = Post::find($id);
+
+        $post->delete();
+        return redirect('/posts');
     }
 }
